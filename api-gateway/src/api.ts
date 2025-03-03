6f8e5960fcdca880ca377e86367b92d811175120
grebrune <grebrune@student.42lyon.fr>
2025-03-03 17:50:20 +0100

    server put in api.js

--- api-gateway/src/api.ts
+++ api-gateway/src/api.ts
@@ -1,25 +1,52 @@
-import fastify from "fastify";
-import httpProxy from '@fastify/http-proxy';
-import cors from "@fastify/cors";
+import { routes } from './routes.js';
+import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import {join} from "node:path";
+import {env} from "./env.js";
+import {readFileSync} from "node:fs";
+import {CreateClient} from "./database.js";
 
-const app = fastify();
+// Load SSL certificates
+// const httpsOptions = {
+//     https: {
+//         key: readFileSync('./key.pem'),      // Private key
+//         cert: readFileSync('./cert.pem')     // Certificate
+//     },
+//     logger: true
+// };
+// const fastify = Fastify(httpsOptions);
+// End of HTTPS setup
 
-app.register(cors, {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"]
+const fastify = Fastify({
+    logger: true
 })
 
-app.register(httpProxy, {
-    upstream: 'http://user-management:8000',
-    prefix: '/user-management',
-    rewritePrefix: '/user-management'
+fastify.register(fastifyStatic, {
+    root: join(import.meta.dirname, "..", "src", "static"),
+    prefix: "/static/"
+})
+
+fastify.get("/*", (req, res) => { // Route pour la page d'accueil
+    const pagePath = join(import.meta.dirname, env.TRANS_VIEWS_PATH, "index.html");
+    const readFile = readFileSync(pagePath, 'utf8');
+    res.type('text/html').send(readFile);
 });
 
-app.listen({port: 8001, host: '0.0.0.0'}, (err, adrr) => {
+
+fastify.setNotFoundHandler((request, reply) => {
+    const pagePath = join(import.meta.dirname, env.TRANS_VIEWS_PATH, "index.html");
+    const readFile = readFileSync(pagePath, 'utf8');
+    reply.type('text/html').send(readFile);
+});
+
+fastify.post("/post/login", CreateClient);
+
+fastify.register(routes)
+
+fastify.listen({ host: '127.0.0.1', port: 3001 }, function (err, address) {
     if (err) {
-        console.error(err);
-        process.exit(1);
+        fastify.log.error(err)
+        process.exit(1)
     }
-    console.log(`server running on ${adrr}`);
-});
-
+    console.log(`Server is now listening on ${address}`)
+})
