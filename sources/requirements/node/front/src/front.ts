9f834350c107179520e92ef75e15349b4855b65b
grebrune <grebrune@student.42lyon.fr>
2025-03-07 12:48:19 +0100

    nice and clear, all working

--- sources/requirements/node/front/src/front.ts
+++ sources/requirements/node/front/src/front.ts
@@ -1,97 +1,84 @@
-// Gérer la navigation lors de l'utilisation des boutons
 document.addEventListener("DOMContentLoaded", () => {
-    // Récupérer les boutons
     const aboutBtn = document.getElementById("about")!;
     const contactBtn = document.getElementById("contact")!;
     const registerBtn = document.getElementById("register")!;
-
-    // Ajouter les écouteurs d'événements sur les boutons
     aboutBtn.addEventListener("click", (event: MouseEvent) => navigate(event, "/about"));
     contactBtn.addEventListener("click", (event: MouseEvent) => navigate(event, "/contact"));
     registerBtn.addEventListener("click", (event: MouseEvent) => navigate(event, "/register"));
-
-    // Charger la page initiale en fonction de l'URL actuelle
     loadPart(window.location.pathname);
 });
 
-// Gérer la navigation via l'historique du navigateur (back/forward)
 window.onpopstate = () => {
     loadPart(window.location.pathname);
 };
 
-// Fonction pour gérer la navigation sans recharger la page
 function navigate(event: MouseEvent, path: string): void {
     event.preventDefault();
-    window.history.pushState({}, "", path);  // Ajout de l'état dans l'historique
-    loadPart(path);  // Chargement du contenu dynamique
+    window.history.pushState({}, "", path);
+    loadPart(path);
 }
 
-// Fonction pour charger les pages dynamiquement
 async function loadPart(page: string): Promise<void> {
     const container = document.getElementById('content') as HTMLElement;
-    if (!page || page === "/") { // Si aucune page n'est spécifiée, afficher un message par défaut
+    if (!page || page === "/") {
+        container.innerHTML = '';
         container.innerHTML = "<p>Choisissez une option pour charger le contenu.</p>";
         return;
     }
-    try { // Tentative de récupération de la page dynamique
-        // insert_tag(container, `part${page}`);
-        const res = await fetch(`part${page}`);
-        const newElement = document.createElement('div');
-        newElement.className = 'tag';
-        if (!res.ok) throw new Error("Page non trouvée");
-        const html = await res.text();  // Récupérer le contenu HTML de la page
-        if (html.includes(container.innerHTML))
-            return;
-        container.innerHTML = '';  // Injecter le contenu dans le div #content
-        newElement.innerHTML = html;  // Injecter le contenu dans le div #content
-        container.appendChild(newElement);
+    try {
+        await insert_tag(`part${page}`);
         if (page === "/register") {
             const button = document.getElementById("registerButton")!;
-            if (button) { // Enable the button
-                button.addEventListener("click", async (event) => {
-                    const myForm = document.getElementById("myForm") as HTMLFormElement;
-                    const formData = new FormData(myForm);
-                    const data: Record<string, unknown> = Object.fromEntries(formData as unknown as Iterable<readonly any[]>);
-                    const response = await fetch('post/create', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(data)
-                    });
-                    const result = await response.json();
-                    console.log(result.redirect);
-                    if (result.redirect) {
-                        const res = await fetch(`${result.redirect}`, {});
-                        const newElement = document.createElement('div');
-                        newElement.className = 'balise';
-                        if (!res.ok) throw new Error("Page non trouvée");
-                        const html = await res.text();  // Récupérer le contenu HTML de la page
-                        container.innerHTML = '';
-                        newElement.innerHTML = html;  // Injecter le contenu dans le div #content
-                        container.appendChild(newElement);
-                    } else {
-                        console.log("Erreur:", result.error);
-                    }
-                });
-            }
+            if (button)
+                register(container, button);
         }
     } catch (error) {
         console.error(error);
+        container.innerHTML = '';
         container.innerHTML = "<h2>404 - Page non trouvée</h2>";
     }
 }
 
+async function insert_tag(url: string): Promise<void>{
+    const container = document.getElementById('content') as HTMLElement;
+    const res = await fetch(url);
+    const newElement = document.createElement('div');
+    newElement.className = 'tag';
+    if (!res.ok)
+        throw Error("Page not found: element missing.");
+    const html = await res.text();
+    if (html.includes(container.innerHTML))
+        return;
+    container.innerHTML = '';
+    newElement.innerHTML = html;
+    container.appendChild(newElement);
+}
 
-// async function insert_tag(container: HTMLElement , url: string): Promise<void>{
-//     console.log("URRRRLL :", url);
-//     const res = await fetch(`part${url}`);
-//     const newElement = document.createElement('div');
-//     newElement.className = 'tag';
-//     if (!res.ok) throw new Error("Page non trouvée");
-//     const html = await res.text();  // Récupérer le contenu HTML de la page
-//     container.innerHTML = '';  // Injecter le contenu dans le div #content
-//     newElement.innerHTML = html;  // Injecter le contenu dans le div #content
-//     container.appendChild(newElement);
-//
-// }
+function register(container: HTMLElement, button: HTMLElement): void {
+    button.addEventListener("click", async (event) => {
+        const myForm = document.getElementById("myForm") as HTMLFormElement;
+        const formData = new FormData(myForm);
+        const data: Record<string, unknown> = Object.fromEntries(formData as unknown as Iterable<readonly any[]>);
+        const response = await fetch('post/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        console.log(result.redirect);
+        if (result.redirect) {
+            const res = await fetch(`${result.redirect}`, {});
+            const newElement = document.createElement('div');
+            newElement.className = 'tag';
+            if (!res.ok)
+                throw new Error("Page not found: redirect missing.");
+            const html = await res.text();
+            container.innerHTML = '';
+            newElement.innerHTML = html;
+            container.appendChild(newElement);
+        } else
+            console.log("Incomplet field or bad request");
+    });
+}
