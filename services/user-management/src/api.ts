5bc41819edc2253bcb0bad979daf33994ea6ea67
grebrune <grebrune@student.42lyon.fr>
2025-03-12 16:24:18 +0100

    remove console log

--- services/user-management/src/api.ts
+++ services/user-management/src/api.ts
@@ -9,7 +9,6 @@ app.post('/sign-up', addUser);
 app.listen({port: 5000, host: '0.0.0.0'}, (err, adrr) => {
     if (err) {
         console.error(err);
-        console.error("In 5000000");
         process.exit(1);
     }
     console.log(`server running on ${adrr}`)
