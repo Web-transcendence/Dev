b21cb05bd202274913c1c82bc757f19e7b08e72b
grebrune <grebrune@student.42lyon.fr>
2025-03-12 16:22:22 +0100

    handle of errors working

--- services/database/src/clientBase.ts
+++ services/database/src/clientBase.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 
 export const Client_db = new Database('client.db')  // Importation correcte de sqlite
 
+// --     DELETE FROM Client; -- remove olds clients and reset database, remove for production
 Client_db.exec(`
     CREATE TABLE IF NOT EXISTS Client (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,50 +22,43 @@ export function emailExist(email: string) {
 }
 
 const clientSchema = z.object({
-    name: z.string().min(2, "Le nom doit contenir au moins 2 caractères"),
-    email: z.string().email("Email invalide"),
-    password: z.string().min(6, "Le mot de passe doit contenir au moins 6 caractères"),
+    name: z.string().min(3, "Minimum 3 caracteres"),
+    email: z.string().email("Invalid email"),
+    password: z.string().min(6, "Minimum 6 caracteres"),
 });
 
 
 export async function createClient(req: FastifyRequest, res: FastifyReply) {
-    // const { name, email, password } = req.body as { name: string; email: string; password: string };
     const mystuff = req.body;
     const zod_result = clientSchema.safeParse(mystuff);
-    console.log("GGGGGGGGGGGGGGGGGGGGGGGGGGGGG");
     if (!zod_result.success) {
-        return zod_result.error.format();
+        console.error(zod_result);
+        return res.status(400).send({json: zod_result.error.format()});
     }
     const { data } = zod_result;
-    console.log("GGGGGGGGGGGGGGGGG1GGGGGGGGGGG");
     let {name, email, password} = data;
     name = sanitizeHtml(name); // Protection from XSS attacks
     email = sanitizeHtml(email);
     password = sanitizeHtml(password);
+    console.log(name, email, password);
     if (!name || !email || !password) {
         return res.status(454).send({error: "All information are required !"});
     }
-    console.log("GGGGGGGGGGGGGGGGGG2GGGGGGGGGGG");
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const insert = Client_db.prepare("INSERT INTO Client (name, email, password) VALUES (?, ?, ?)");
     const result = insert.run(name, email, hashedPassword);
-    const rows = Client_db.prepare(`SELECT * FROM clients`).all();
-    console.log("GGGGGGGGGGGGGGGG44444GGGGGGGGGGGGG");
-    console.table(rows);
+    // const rows = Client_db.prepare(`SELECT * FROM Client`).all(); // Print clients info
+    // console.table(rows);
     if (result.changes === 1) {
-        // const table = Client_db.prepare("SELECT * FROM clients");
-        // console.table(table.database);
         Client_db.prepare("SELECT * FROM Client");
         const newClientId = result.lastInsertRowid;
         const newClient = Client_db.prepare("SELECT name FROM Client WHERE id = ?").get(newClientId) as { name: string } | undefined;
         if (newClient) {
-            console.log("Nouveau client enregistré:", newClient.name);
+            console.log("New client registred:", newClient.name);
         }
-        return res.redirect(`/part/login?name=${encodeURIComponent(newClient?.name || "Utilisateur")}`);
-        // return reply.redirect(`/part/login`);
-        // return reply.status(201).send({ message: "Compte client créé avec succès", id: result.lastInsertRowid });
+        return res.status(201).send({ id: result.lastInsertRowid });
     } else {
-        return res.status(500).send({ error: "Erreur lors de la création du compte" });
+        return res.status(500).send({ error: "Error from database " });
     }
 }
