08642cb2d5d07fdd86c657f5664120cfc26c508a
grebrune <grebrune@student.42lyon.fr>
2025-03-14 11:15:04 +0100

    Scope Missing

--- services/user-management/src/addUser.ts
+++ services/user-management/src/addUser.ts
@@ -2,7 +2,7 @@ import {FastifyReply, FastifyRequest} from "fastify";
 import jwt from 'jsonwebtoken';
 
 export async function addUser(req: FastifyRequest, res: FastifyReply):Promise<void> {
-    const { name, email, password } = req.body as { name: string; email: string; password: string };
+    const {name, email, password} = req.body as { name: string; email: string; password: string };
     try {
         const response = await fetch(`http://database:4001/email-existing?email=${encodeURIComponent(email)}`, {
             method: 'GET',
@@ -11,7 +11,7 @@ export async function addUser(req: FastifyRequest, res: FastifyReply):Promise<vo
             }
         });
         if (!response.ok) {
-            return res.status(400).send({ error: "Email déjà utilisé" });
+            return res.status(400).send({error: "Email déjà utilisé"});
         }
         console.log(response);
         const addUserRes = await fetch('http://database:4001/addUser', {
@@ -28,10 +28,10 @@ export async function addUser(req: FastifyRequest, res: FastifyReply):Promise<vo
         });
         if (!addUserRes.ok) {
             const errorData = await addUserRes.json();
-            return res.status(500).send({ json: errorData});
+            return res.status(500).send({json: errorData});
         }
-        const user = { id: 1, name: name };
-        const token = jwt.sign(user, 'secret_key', { expiresIn: '1h' });
+        const user = {id: 1, name: name};
+        const token = jwt.sign(user, 'secret_key', {expiresIn: '1h'});
         res.status(201).send({token, username: name, redirect: 'post/login'});
     } catch (err) {
         console.error(err);
