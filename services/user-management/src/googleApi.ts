a53c835759ca39fe77115eb4ea133a62b9f620da
grebrune <grebrune@student.42lyon.fr>
2025-03-18 14:37:49 +0100

    img google working

--- services/user-management/src/googleApi.ts
+++ services/user-management/src/googleApi.ts
@@ -2,7 +2,6 @@ import {FastifyReply, FastifyRequest} from "fastify";
 import { OAuth2Client } from 'google-auth-library';
 import { Client_db} from "./User.js";
 import jwt from "jsonwebtoken";
-import {string} from "zod";
 
 // Initialisation du client Google OAuth2
 const client = new OAuth2Client("562995219569-0icrl4jh4ku3h312qmjm8ek57fqt7fp5.apps.googleusercontent.com");
@@ -28,20 +27,17 @@ export async function googleAuth(request: FastifyRequest, reply: FastifyReply):P
         console.log('User ID:', userId);
         console.log('Email:', payload.email);
         console.log('Name:', payload.name);
+        console.log('Profile Picture URL:', payload.picture);  // L'URL de la photo de profil
 
-        if (Client_db.prepare("SELECT * FROM Client WHERE email = ?").get(payload.email)) {
+        if (Client_db.prepare("SELECT * FROM Client WHERE email = ?").get(payload.email))
             console.log('Email Already Register:', payload.email);
-
-            return reply.send({token, valid: true, name: payload.name});
-        }
         else {
             const res = Client_db.prepare("INSERT INTO Client (name, email, password) VALUES (?, ?, ?)")
                 .run(payload.name, payload.email, '');
+            console.log('New Email:', payload.email);
         }
-        // Réponse réussie
-        const token = jwt.sign({ name: payload.name, email: payload.email, userId: payload.sub }, 'secret_key', { expiresIn: '1h' });
-        // const token = jwt.sign(payload.name, 'secret_key', { expiresIn: '1h' });
-        return reply.send({token, valid: true, name: payload.name});
+        const token = jwt.sign({ name: payload.name, email: payload.email, avatar: payload.picture, userId: payload.sub }, 'secret_key', { expiresIn: '1h' });
+        return reply.send({token, valid: true, name: payload.name, avatar: payload.picture});
     } catch (error) {
         console.log('Error verifying Google token:', error);
         reply.status(400).send({ success: false, error: 'Invalid token' });
