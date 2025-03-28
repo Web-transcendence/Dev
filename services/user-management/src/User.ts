58ea41b62f287425cb44acb56346e6fdbe1cb7f9
avaldin <valdinalan.pro@gmail.com>
2025-03-28 12:02:01 +0100

    friend list, invitation, and getFriendList method are done but not tested.

--- services/user-management/src/User.ts
+++ services/user-management/src/User.ts
@@ -15,6 +15,16 @@ Client_db.exec(`
     )
 `);
 
+Client_db.exec(`
+    CREATE TABLE IF NOT EXISTS FriendListe (
+        userA_id INTEGER NOT NULL,
+        userB_id INTEGER NOT NULL,
+        status TEXT check(status IN ('pending', 'accepted')) DEFAULT ('pending'),
+        PRIMARY KEY (userA_id, serB_id),
+        FOREIGN KEY (userA_id) REFERENCES users(id) ON DELETE CASCADE,
+        FOREIGN KEY (userB_id) REFERENCES users(id) ON DELETE CASCADE,
+)`)
+
 export class User {
     id: string;
 
@@ -87,4 +97,32 @@ export class User {
             return console.log("Server error: res not found in connectedUsers");
         res.sse({data: JSON.stringify({event: "invite", data: "teeest"})});
     }
+
+    addFriend(nickName: string): {code: number, result: string} {
+        const friendId = Client_db.prepare("SELECT id FROM Client WHERE nickName = ?").get(nickName);
+        if (!friendId)
+            return {code: 409, result: "this nickName doesn't exist"};
+
+        const checkStatus = Client_db.prepare("SELECT status FROM FriendList WHERE userA_id = ? AND userB_id = ?").get(friendId, this.id);
+        if (checkStatus == 'accepted')
+            return {code: 409, result: "Friend already added"};
+
+        else if (checkStatus == 'pending') {
+            Client_db.prepare(`UPDATE FriendList SET status = 'accepted' WHERE userA_id = ? AND userB_id = ?`).run(friendId, this.id);
+            return {code: 201, result: "Friend invitation accepted"};
+        }
+
+        const res = Client_db.prepare(`INSERT OR IGNORE INTO FriendList (userA_id, userB_id, status) VALUES (?, ?, ?)`).run(this.id, friendId);
+        if (!res)
+            return {code: 409, result: "Friend invitation already sent"};
+        return {code: 201, result: `friend invitation sent successfully`};
+    }
+
+    getFriendList(): {accepted: { id: number }[], pending: { id: number }[], invited: { id: number }[]} {
+        const accepted = Client_db.prepare(`SELECT userA_id FROM FriendList WHERE userB_id = ? AND status = 'accepted' UNION SELECT userB_id FROM FriendList WHERE userA_id = ? AND status = 'accepted'`).all(this.id) as {id: number}[];
+        const pending = Client_db.prepare(`SELECT userB_id FROM FriendList WHERE userA_id = ? AND status = 'pending'`).all(this.id) as {id: number}[];
+        const invited = Client_db.prepare(`SELECT userA_id FROM FriendList WHERE userB_id = ? AND status = 'pending'`).all(this.id) as {id: number}[];
+        return {accepted, pending, invited};
+    }
+
 }
