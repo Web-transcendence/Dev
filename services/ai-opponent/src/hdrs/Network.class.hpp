0e0bc7a1c6b0e7a0603c0d53269bd8d8ccfa6cf9
Thibaud Mouche <tmouche@z4r9p2.42lyon.fr>
2025-04-10 19:35:32 +0200

    the network must be initialized by a .json file

--- services/ai-opponent/src/hdrs/Network.class.hpp
+++ services/ai-opponent/src/hdrs/Network.class.hpp
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   Network.class.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tmouche <tmouche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:33:52 by thibaud           #+#    #+#             */
-/*   Updated: 2025/04/10 09:21:12 by thibaud          ###   ########.fr       */
+/*   Updated: 2025/04/10 19:31:53 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NETWORK_CLASS_HPP
 # define NETWORK_CLASS_HPP
-# define N_LAYER_HIDDEN 2
-# define N_NEURON_INPUT 8
-# define N_NEURON_OUTPUT 2
-# define N_NEURON_HIDDEN 250
+# define N_LAYER_HIDDEN 1
+# define N_NEURON_INPUT 16
+# define N_NEURON_OUTPUT 4
+# define N_NEURON_HIDDEN 25
 
 #include <string>
 #include <vector>
@@ -26,6 +26,8 @@ public:
 	Network(std::string const & inFile);
 	~Network( void ) {}
 
+	std::vector<double>	feedForward(std::vector<double> const & input);
+	
 private:
 	Network( void ) {}
 	
--- services/ai-opponent/src/srcs/Network.cpp
+++ services/ai-opponent/src/srcs/Network.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Network.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tmouche <tmouche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:45:44 by thibaud           #+#    #+#             */
-/*   Updated: 2025/04/10 09:21:02 by thibaud          ###   ########.fr       */
+/*   Updated: 2025/04/10 19:33:50 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include "json.hpp"
 
 #include <fstream>
+#include <sstream>
+
+#include <iostream>
 
 Network::Network(std::string const & inFile) {
 	std::string	dataStr;
@@ -22,24 +25,47 @@ Network::Network(std::string const & inFile) {
 	if (!ifs)
 		throw std::exception();
 	std::getline(ifs, dataStr, '\0');
-	auto data = nlohmann::json::parse(dataStr);
+	ifs.close();
+	auto data = nlohmann::json::parse(dataStr)["Network"];
 	std::vector<unsigned int>	sizes(N_LAYER_HIDDEN + 2);
 	sizes.at(0) = N_NEURON_INPUT;
-	for (unsigned int i = 1; i < N_LAYER_HIDDEN; i++)
+	for (unsigned int i = 1; i < N_LAYER_HIDDEN + 1; i++)
 		sizes.at(i) = N_NEURON_HIDDEN;
 	sizes.at(N_LAYER_HIDDEN + 1) = N_NEURON_OUTPUT;
 	this->_weights = std::vector<std::vector<std::vector<double>>>(N_LAYER_HIDDEN + 1);
 	this->_biaises = std::vector<std::vector<double>>(N_LAYER_HIDDEN + 1);
-	auto			it_wl = this->_weights.begin();
-	auto			it_bl = this->_biaises.begin();
-	unsigned int	whichLayer = 1;
-	for (; it_wl != this->_weights.end(); it_wl++, it_bl++, whichLayer++) {
+	auto				it_wl = this->_weights.begin();
+	auto				it_bl = this->_biaises.begin();
+	unsigned int		whichLayer = 1;
+	unsigned int		countLayer = 0;
+	for (; it_wl != this->_weights.end(); it_wl++, it_bl++, whichLayer++, countLayer++) {
+		std::stringstream	ssLayer;
+		unsigned int	countNeuron = 0;
 		*it_wl = std::vector<std::vector<double>>(sizes.at(whichLayer));
 		*it_bl = std::vector<double>(sizes.at(whichLayer));
+		ssLayer << "Layer " << countLayer;
 		auto	it_wn = (*it_wl).begin();
-		for (;it_wn != (*it_wl).end(); it_wn++)
+		auto	it_bn = (*it_bl).begin();
+		for (;it_wn != (*it_wl).end(); it_wn++, it_bn++, countNeuron++) {
+			std::stringstream	ssNeuron;
+			ssNeuron << "neuron " << countNeuron;
 			*it_wn = std::vector<double>(sizes.at(whichLayer - 1));
+			unsigned int	countWeight = 0;
+			for (auto it_ww = (*it_wn).begin(); it_ww != (*it_wn).end(); it_ww++, countWeight++) {
+				std::stringstream	w;
+				w << data[ssLayer.str()][ssNeuron.str()]["w"][countWeight];
+				w >> *it_ww;
+			}
+			std::stringstream	b;
+			b << data[ssLayer.str()][ssNeuron.str()]["b"];
+			b >> *it_bn;
+		}
 	}
 	return ;
 }
 
+std::vector<double>	Network::feedForward(std::vector<double> const & input) {
+	std::vector<double>	res(N_NEURON_OUTPUT);
+
+	
+}
