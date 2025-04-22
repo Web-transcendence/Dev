c8d04a0d67a1f0655bd023a0412f7331cac4fe7f
thibaud <thibaud.tme@gmail.com>
2025-04-22 19:27:51 +0200

    add a test server to debug the factory/client and AI server

--- services/ai-opponent/src/core/Client/srcs/main.cpp
+++ services/ai-opponent/src/core/Client/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:37:11 by thibaud           #+#    #+#             */
-/*   Updated: 2025/04/20 14:53:08 by thibaud          ###   ########.fr       */
+/*   Updated: 2025/04/22 19:22:08 by thibaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 
 int	main(int ac, char** av) {
 	if (ac != 2) {
-		std::cout << "Error: Parameters: ./Facory <Game Server Ws>" << std::endl;
+		std::cout << "Error: Parameters: ./Factory <Game Server Ws>" << std::endl;
 		return 1;
 	}
 	try {
 		Factory	myFactory(av[1]);
+		std::cout << "Factory should be connected" << std::endl;
 		myFactory.run();
 	} catch (std::exception const & e) {
 		std::cout << "Error: " << e.what() << std::endl;
