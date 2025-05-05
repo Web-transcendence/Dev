9e2b39c2b7b2de262bf87b7498be48563cfa0850
Gregoirebrn <150047639+Gregoirebrn@users.noreply.github.com>
2025-05-05 19:01:50 +0200

    feat: message can change in the notification

--- services/user-management/src/googleApi.ts
+++ services/user-management/src/googleApi.ts
@@ -1,7 +1,7 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import { OAuth2Client } from 'google-auth-library';
-import { Client_db} from "./User.js";
-import jwt from "jsonwebtoken";
+import {Client_db, User} from "./User.js";
+import {DataBaseError} from "./error.js";
 
 const client = new OAuth2Client("562995219569-0icrl4jh4ku3h312qmjm8ek57fqt7fp5.apps.googleusercontent.com");
 
@@ -25,9 +25,10 @@ export async function googleAuth(request: FastifyRequest, reply: FastifyReply):P
             const res = Client_db.prepare("INSERT INTO Client (nickName, email, password, google_id, pictureProfile) VALUES (?, ?, ?, ?, ?)")
                 .run(payload.given_name, payload.email, 'NOTGIVEN', userId, payload.picture);
         }
-        const userData = Client_db.prepare("SELECT id FROM Client WHERE email = ?").get(payload.email) as {id: string};
-
-        const token = jwt.sign({ id: userData.id, name: payload.given_name, email: payload.email, avatar: payload.picture, userId: payload.sub }, 'secret_key', { expiresIn: '1h' });
+        const userData = Client_db.prepare("SELECT id FROM Client WHERE email = ?").get(payload.email) as {id: number} | undefined;
+        if (!userData)
+            throw new DataBaseError('cannot recover id from user connected by google', 'Internal server error', 500)
+        const token = User.makeToken(userData.id)
         return reply.send({token, valid: true, nickName: payload.given_name, avatar: payload.picture});
     } catch (error) {
         console.log('Error verifying Google token:', error);
