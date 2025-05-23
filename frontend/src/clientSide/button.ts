5064a8f9d50ee6b7d58a0f65693471dac9b27742
avaldin <valdinalan.pro@gmail.com>
2025-05-23 13:09:33 +0200

    fix: brackets now call fetchUserInformation only for players with id != 0

--- frontend/src/clientSide/button.ts
+++ frontend/src/clientSide/button.ts
@@ -1,11 +1,11 @@
-import { addFriend, init2fa, login, profile, register, setAvatar, verify2fa } from './user.js'
+import { addFriend, fetchUserInformation, init2fa, login, profile, register, setAvatar, verify2fa } from './user.js'
 import { friendList } from './friends.js'
 import { connected, handleConnection, navigate } from './front.js'
 import { tdStop, TowerDefense } from './td.js'
 import { editProfile } from './editInfoProfile.js'
 import { displayNotification } from './notificationHandler.js'
 import { Pong } from './pong.js'
-import { displayTournaments, joinTournament, launchTournament } from './tournaments.js'
+import { displayTournaments, fetchTournamentBrackets, joinTournament, launchTournament } from './tournaments.js'
 import { printMatchHistory } from './matchHistory.js'
 import { TowerDefenseSpec } from './tdspec.js'
 import { closeSSEConnection } from './serverSentEvent.js'
@@ -28,7 +28,7 @@ const mapButton: { [key: string]: () => void } = {
 	'/lobby': lobby,
 	'/matchHistory': matchHistory,
 //	'/toKnow': toKnow,
-	'/pongVsia': pongAgainstAi
+	'/pongVsia': pongAgainstAi,
 	'/About': About,
 	'/brackets': Brackets,
 }
@@ -69,7 +69,7 @@ async function profileBtn() {
 				await addFriend(addFriendIpt.value)
 				await friendList()
 			} else {
-				displayNotification('Name of 3 characters minimum', {type: 'error'})
+				displayNotification('Name of 3 characters minimum', { type: 'error' })
 			}
 		})
 	const activeFA = sessionStorage.getItem('activeFA')
@@ -128,7 +128,7 @@ function factor() {
 		const input = document.getElementById('inputVerify') as HTMLInputElement | null
 		if (!input) {
 			await navigate('/logout')
-			displayNotification('Error from client, try again!', {type: 'error'})
+			displayNotification('Error from client, try again!', { type: 'error' })
 			return
 		}
 		await verify2fa(input.value, 'The validity of your code has been confirmed')
@@ -170,10 +170,10 @@ function towerWatch() {
 
 function tournaments() {
 	const tournaments: { id: number, name: string } [] = [
-		{id: 4, name: 'Junior'},
-		{id: 8, name: 'Contender'},
-		{id: 16, name: 'Major'},
-		{id: 32, name: 'Worlds'}]
+		{ id: 4, name: 'Junior' },
+		{ id: 8, name: 'Contender' },
+		{ id: 16, name: 'Major' },
+		{ id: 32, name: 'Worlds' }]
 	for (const parse of tournaments)
 		document.getElementById(`${parse.id}`)
 			?.addEventListener('click', async (event) => {
@@ -219,33 +219,36 @@ async function Brackets() {
 	try {
 		const idTournament = sessionStorage.getItem('idTournaments')
 		if (!idTournament) throw new Error('No ID Tournament found!')
-		const bracketsData = await fetchTournamentBrackets(Number(idTournament));
-		console.log('BRACKETS DATA', bracketsData);
-		if (!bracketsData) throw new Error("No brackets data");
+		const bracketsData = await fetchTournamentBrackets(Number(idTournament))
+		console.log('BRACKETS DATA', bracketsData)
+		if (!bracketsData) throw new Error('No brackets data')
 
-		const template = document.getElementById("bracketsTmp") as HTMLTemplateElement | null;
-		const list = document.getElementById("playerList") as HTMLUListElement | null;
+		const template = document.getElementById('bracketsTmp') as HTMLTemplateElement | null
+		const list = document.getElementById('playerList') as HTMLUListElement | null
 
-		if (!template || !list) throw new Error("Missing template or list");
+		if (!template || !list) throw new Error('Missing template or list')
 
 		for (const bracket of bracketsData) {
-			const userData = await fetchUserInformation([bracket.id1, bracket.id2]);
-			if (!userData) continue;
+			const playerId: number[] = []
+			if (bracket.id1 !== 0) playerId.push(bracket.id1)
+			if (bracket.id2 !== 0) playerId.push(bracket.id2)
+			const userData = await fetchUserInformation(playerId)
+			if (!userData) continue
 
-			const clone = template.content.cloneNode(true) as DocumentFragment;
+			const clone = template.content.cloneNode(true) as DocumentFragment
 
-			const playerOneImg = clone.querySelector(".player-one-img") as HTMLImageElement | null;
-			const playerOneName = clone.querySelector(".player-one-name") as HTMLElement | null;
-			const playerTwoImg = clone.querySelector(".player-two-img") as HTMLImageElement | null;
-			const playerTwoName = clone.querySelector(".player-two-name") as HTMLElement | null;
-			if (playerOneImg && userData[0].avatar) playerOneImg.src = userData[0].avatar;
-			if (playerOneName) playerOneName.innerText = userData[0].nickName;
-			if (playerTwoImg && userData[1].avatar) playerTwoImg.src = userData[1].avatar;
-			if (playerTwoName) playerTwoName.innerText = userData[1].nickName;
-			list.appendChild(clone);
+			const playerOneImg = clone.querySelector('.player-one-img') as HTMLImageElement | null
+			const playerOneName = clone.querySelector('.player-one-name') as HTMLElement | null
+			const playerTwoImg = clone.querySelector('.player-two-img') as HTMLImageElement | null
+			const playerTwoName = clone.querySelector('.player-two-name') as HTMLElement | null
+			if (playerOneImg && userData[0]?.avatar) playerOneImg.src = userData[0].avatar
+			if (playerOneName) playerOneName.innerText = userData[0]?.nickName
+			if (playerTwoImg && userData[1]?.avatar) playerTwoImg.src = userData[1].avatar
+			if (playerTwoName) playerTwoName.innerText = userData[1]?.nickName
+			list.appendChild(clone)
 		}
 	} catch (error) {
-		console.log('Brackets Error: ', error);
-		displayNotification("Can't show phase of tournament", { type: "error" });
+		console.log('Brackets Error: ', error)
+		displayNotification('Can\'t show phase of tournament', { type: 'error' })
 	}
 }
